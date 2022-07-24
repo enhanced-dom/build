dc0474e199d1538cb41963c27c05eaa9cbed213e
Enhanced Dom <enhanced.dom@gmail.com>
2022-07-24 21:38:21 +0200

    corrected use of yargs; linting on commit with husky; included sourcemaps

--- src/copyPackageJson.ts
+++ src/copyPackageJson.ts
@@ -3,9 +3,13 @@ import path from 'path'
 import fs from 'fs'
 import yargs from 'yargs'
 const args = yargs(process.argv)
-  .usage('$0 <folder>')
-  .positional('folder', { describe: 'destination folder for package.json copy', type: 'string' })
-  .default('folder', 'dist').argv as {folder: string}
-const packageJSONContents = require(path.join(process.cwd(), 'package.json'))
+  .usage('$0 -folder <folder> -file <source package.json path>')
+  .nargs('folder', 1)
+  .describe('folder', 'Destination folder. Default: ./dist')
+  .default('folder', 'dist')
+  .nargs('file', 1)
+  .describe('file', 'Source package.json file path. Default: ./package.json')
+  .default('file', 'package.json').argv as {folder: string; file: string}
+const packageJSONContents = require(path.join(process.cwd(), args.file))
 delete packageJSONContents.scripts
 fs.writeFileSync(path.join(process.cwd(), args.folder, 'package.json'), JSON.stringify(packageJSONContents, null, 2))
